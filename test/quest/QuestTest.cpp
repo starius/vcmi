7d4e01f00f27bf1cc99ed33b97feadc0047d1347
Boris Nagaev <bnagaev@gmail.com>
2026-08-04 20:00:03 -0500

    Consolidate tiny-map test setup
    Use the shared live-game fixture in quest and Nullkiller tests instead
    of repeating map loading, callback, visibility, and lookup code.

--- test/quest/QuestTest.cpp
+++ test/quest/QuestTest.cpp
@@ -12,108 +12,12 @@
 #include "QuestTest.h"
 
 #include "../../lib/CPlayerState.h"
-#include "../../lib/callback/CCallback.h"
 #include "../../lib/mapObjects/CGHeroInstance.h"
 #include "../../lib/mapObjects/CGObjectInstance.h"
 
-void QuestTest::startWithMap(TinyH3M::TinyH3MBuilder builder)
+void QuestTest::onMapStarted()
 {
-	startWithMap(std::move(builder), EMapDifficulty::EASY);
-}
-
-void QuestTest::startWithMap(TinyH3M::TinyH3MBuilder builder, EMapDifficulty difficulty)
-{
-	const auto * info = ::testing::UnitTest::GetInstance()->current_test_info();
-	std::string fixtureName = std::string(info->test_suite_name()) + "_" + info->name();
-	std::replace(fixtureName.begin(), fixtureName.end(), '/', '_'); // parameterized cases embed '/'
-	auto bytes = builder.buildAndDump(fixtureName);
-	mapService = std::make_unique<MapServiceTinyH3M>(std::move(bytes), this);
-
-	StartInfo si;
-	si.mapname    = "tiny";
-	si.difficulty = static_cast<ui8>(difficulty);
-	si.mode       = EStartMode::NEW_GAME;
-
-	auto header = mapService->loadMapHeader(ResourcePath(si.mapname));
-	ASSERT_NE(header.get(), nullptr) << "TinyH3M scenario header failed to load";
-
-	for(int i = 0; i < static_cast<int>(header->players.size()); ++i)
-	{
-		const PlayerInfo & pinfo = header->players[i];
-		if(!(pinfo.canHumanPlay || pinfo.canComputerPlay))
-			continue;
-
-		PlayerSettings & pset = si.playerInfos[PlayerColor(i)];
-		pset.color  = PlayerColor(i);
-		pset.connectedPlayerIDs.insert(static_cast<PlayerConnectionID>(i));
-		pset.name   = "Player";
-		pset.castle = pinfo.defaultCastle();
-		pset.hero   = pinfo.defaultHero();
-	}
-
-	GameRandomizer randomizer(*gameState);
-	Load::ProgressAccumulator progressTracker;
-	gameState->init(mapService.get(), &si, randomizer, progressTracker, false);
-
-	ASSERT_NE(map, nullptr) << "gameState init did not populate the CMap";
-
-	// Needed by mock takeCreatures and any override that resolves ObjectInstanceIDs.
 	gameEventCallback->setGameInfoCallback(gameState.get());
-}
-
-CGObjectInstance * QuestTest::findObjectAt(const int3 & pos) const
-{
-	for(const auto & obj : map->objects)
-	{
-		if(obj && obj->anchorPos() == pos)
-			return obj.get();
-	}
-	return nullptr;
-}
-
-CGHeroInstance * QuestTest::findHeroAt(const int3 & pos) const
-{
-	for(const auto & obj : map->objects)
-	{
-		auto * hero = dynamic_cast<CGHeroInstance *>(obj.get());
-		if(hero && hero->anchorPos() == pos)
-			return hero;
-	}
-	return nullptr;
-}
-
-CGHeroInstance * QuestTest::findHeroByOwner(PlayerColor owner) const
-{
-	for(const auto & obj : map->objects)
-	{
-		auto * hero = dynamic_cast<CGHeroInstance *>(obj.get());
-		if(hero && hero->getOwner() == owner)
-			return hero;
-	}
-	return nullptr;
-}
-
-void QuestTest::revealMap(PlayerColor player)
-{
-	auto * team = gameState->getPlayerTeam(player);
-	ASSERT_NE(team, nullptr);
-
-	for(int z = 0; z < map->levels(); ++z)
-		for(int x = 0; x < map->width; ++x)
-			for(int y = 0; y < map->height; ++y)
-				team->fogOfWarMap[int3(x, y, z)] = 1;
-}
-
-std::shared_ptr<CCallback> QuestTest::makeCallback(PlayerColor player, IClient * client) const
-{
-	return std::make_shared<CCallback>(gameState, std::optional<PlayerColor>{ player }, client);
-}
-
-void QuestTest::grantResources(PlayerColor player, GameResID which, int amount)
-{
-	auto it = gameState->players.find(player);
-	ASSERT_NE(it, gameState->players.end()) << "Player " << player.getNum() << " not initialised";
-	it->second.resources[which] += amount;
 }
 
 void QuestTest::markObjectDestroyed(PlayerColor player, ObjectInstanceID target)
@@ -143,11 +47,6 @@ void QuestTest::answerDialog(CGHeroInstance * hero, int32_t answer)
 	ASSERT_NE(captured.caller, nullptr)
 		<< "captured BlockingDialog has no caller; blockingDialogAnswered would have nothing to dispatch on";
 	captured.caller->blockingDialogAnswered(*gameEventCallback, hero, answer);
-}
-
-void QuestTest::overrideSettingBeforeInit(EGameSettings option, bool value)
-{
-	pendingOverrides.push_back({option, value});
 }
 
 void QuestTest::advanceDays(int days)
